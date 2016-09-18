8b49621f04123018937aea5882f7c0ae7c2985d9
Attila Gavaller <backdoor.werewolf@gmail.com>
2016-09-18 12:22:28 +0200

    hello World!

--- src/main/java/example/app/MainApplication.java
+++ src/main/java/example/app/MainApplication.java
@@ -3,7 +3,7 @@ package example.app;
 public class MainApplication {
 	
     public static void main(String[] args) {    	
-    	String helloMsg = "Hello Hibernate!";		
+    	String helloMsg = "Hello World!";		
 		System.out.println(helloMsg);		
 	}  
 
